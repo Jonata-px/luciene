47b0488b28b9a3b46edf41e180c36a8b2b78f94b
Jonatã Felix <jonatafelixc@gmail.com>
2023-06-06 18:34:20 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   experimental: {
     appDir: true,
   },
-  distDir: 'out',
 }
 
 module.exports = nextConfig
