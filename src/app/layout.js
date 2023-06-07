8327f794bca5b70734d61f52b8cb69b5b4640719
Jonatã Felix <jonatafelixc@gmail.com>
2023-06-07 11:20:25 -0300

    icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,16 @@ export const metadata = {
   description: 'Encontre equilíbrio, relaxamento e bem-estar com a reflexologia podal em Pinhalzinho, SP. Com especialização em tratamentos de reflexologia podal para promover saúde, aliviar tensões e melhorar a circulação. Agende uma sessão hoje mesmo!',
   keywords:'reflexologia podal, reflexologia, bem-estar, saúde, relaxamento, terapia natural, Pinhalzinho, SP',
   author:"Jfcoder",
+  icons: {
+    icon: '/icon.ico',
+  },
 }
 
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body>
             {children}
           <Footer/>
